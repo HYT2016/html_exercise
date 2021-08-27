4360d1e6ce1d5e06c4d7c97137308b832e2e0073
hsz <sgsam42594@gmail.com>
2021-08-27 23:33:31 +0800

    revise pets_shop

--- pets_shop/README.markdown
+++ pets_shop/README.markdown
@@ -1,5 +1,5 @@
 # 
 
-A Pen created on CodePen.io. Original URL: [https://codepen.io/SZHuang/pen/MWoWXBY](https://codepen.io/SZHuang/pen/MWoWXBY).
+A Pen created on CodePen.io. Original URL: [https://codepen.io/SZHuang/pen/rNwVbrd](https://codepen.io/SZHuang/pen/rNwVbrd).
 
 
---
+++ pets_shop/src/script.js
@@ -0,0 +1,22 @@
+$(".product_name").click(
+  function() {
+    if ($(this).attr("data-cata")=="dog") {
+      $(".show_cata").text("BUY: 要買狗狗嗎");
+      
+    }else if ($(this).attr("data-cata")=="rabbit") {
+      $(".show_cata").text("BUY: 要買兔兔嗎");
+    }else if ($(this).attr("data-cata")=="cat") {
+      $(".show_cata").text("BUY: 要買貓咪嗎");
+    }else if ($(this).attr("data-cata")=="pig") {
+      $(".show_cata").text("BUY: 要買豬豬嗎");
+    }
+    // $(this).css("width", 125);
+    setTimeout(
+      function() {
+        $(".show_cata").text("BUY");
+        
+      }
+    , 1000);
+  }
+);
+
